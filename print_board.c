75186ca36dd6bb991957c1eb0bb7c701d4f1cad7
Vitalii Heneha <genlosha@gmail.com>
2025-03-12 15:34:54 +0100

    nice print_board

--- print_board.c
+++ print_board.c
@@ -7,17 +7,27 @@ int print_board(piece_t board[8][8]) {
     extern int row;
 
     // print board
-    for (row = 7; row >= 0; row--) {
-        printf("%d ", row+1);
-        for (column = 0; column < 8; column++) {
-            printf("[%c-%c]", board[column][row].type, board[column][row].color);
-        }
-        printf("\n");
-    }
+    printf("\n ");
     for (column = 0; column < 8; column++) {
-        printf("    %c", column+65);
+        printf("   %c", column+65);
     }
     printf("\n");
+    for (row = 7; row >= 0; row--) {
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("%d ", row+1);
+        for (column = 0; column < 8; column++) {
+            printf("|%c %c", board[column][row].type, board[column][row].color);
+        }
+        printf("|");
+        printf(" %d", row+1);
+        printf("\n");
+    }
+    printf("  +---+---+---+---+---+---+---+---+\n");
+    printf(" ");
+    for (column = 0; column < 8; column++) {
+        printf("   %c", column+65);
+    }
+    printf("\n\n");
 
     return(0);
 }
