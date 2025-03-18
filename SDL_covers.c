f098ed8870e552fa03bc779fa9c1245095d14009
Anastasiia Polevikova <anastasiia.polevikova@gmail.com>
2025-03-18 23:37:18 +0100

    added hints on start & end window

--- SDL_covers.c
+++ SDL_covers.c
@@ -32,10 +32,6 @@ int SDL_covers(int posit_in_game) {
 		SDL_Log("Oberfläche nicht erhalten! SDL_Error: %s\n", SDL_GetError());
 		return -1;
 	}
-
-	// Farben erstellen (int)
-	Sint32 white = SDL_MapRGBA(surface->format, 255, 255, 255, 255);
-	Sint32 grey = SDL_MapRGBA(surface->format, 128, 128, 128, 255);
 	
 	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 128, 128, 128));
 
@@ -45,19 +41,24 @@ int SDL_covers(int posit_in_game) {
 	}
 
 	TTF_Font *font = TTF_OpenFont("Go_Mono.ttf", 45);
+	TTF_Font *small_font = TTF_OpenFont("Go_Mono.ttf", 30);
+
 	if (font == NULL){
 		SDL_Log("Font could not be loaded. SDL_ttf Error: %s\n", TTF_GetError());
 		return -1;
 	}
 	
-	// white color (SDL_Color)
+	// black color (SDL_Color)
 	SDL_Color black_color = {.r = 0, .g = 0, .b = 0, .a = 255};
 	SDL_Surface *text = NULL;
+	SDL_Surface *small_text = NULL;
 	//im callinf this function only twice with explicit values as parameter, no 3rd case is possible ?
 	if (posit_in_game == 0) {
 		// from text to pixels
 		text = TTF_RenderText_Solid(font, "- RPG CHESS - ", black_color);
-		if (text == NULL) {
+		small_text = TTF_RenderText_Solid(small_font, "ESC|leave & ENTER|start game", black_color);
+
+		if (text == NULL || small_text == NULL) {
 			SDL_Log("Text rendering failed: %s\n", TTF_GetError());
 			TTF_CloseFont(font);
 			return -1;
@@ -66,21 +67,29 @@ int SDL_covers(int posit_in_game) {
 	else {
 		// from text to pixels
 		text = TTF_RenderText_Solid(font, "- THE END - ", black_color);
-		if (text == NULL) {
+		small_text = TTF_RenderText_Solid(font, "ESC|leave", black_color);
+		
+		if (text == NULL || small_text == NULL) {
 			SDL_Log("Text rendering failed: %s\n", TTF_GetError());
 			TTF_CloseFont(font);
+			TTF_CloseFont(small_font);
 			return -1;
 		}
 	}
 	
 	SDL_Surface *text_konvertiert = SDL_ConvertSurfaceFormat(text, surface->format->format, 0);
-	if (text_konvertiert == NULL) {
+	SDL_Surface *small_text_konvertiert = SDL_ConvertSurfaceFormat(small_text, surface->format->format, 0);
+	
+	if (text_konvertiert == NULL ||small_text_konvertiert == NULL) {
 		SDL_Log("Text konnte nicht konvertiert werden! SDL_Error Error: %s\n", SDL_GetError());
 		SDL_FreeSurface(text);
+		SDL_FreeSurface(small_text);
 		TTF_CloseFont(font);
+		TTF_CloseFont(small_font);
 		return -1;
 	}
 	SDL_FreeSurface(text);
+	SDL_FreeSurface(small_text);
 
 	// Define a destination rectangle to center the text
 	SDL_Rect text_rect;
@@ -89,8 +98,15 @@ int SDL_covers(int posit_in_game) {
 	text_rect.w = text_konvertiert->w;
 	text_rect.h = text_konvertiert->h;
 
+	SDL_Rect small_text_rect;
+	small_text_rect.x = (SCREEN_WIDTH - small_text_konvertiert->w) / 2;
+	small_text_rect.y = text_rect.y + text_rect.h + 10;
+	small_text_rect.w = small_text_konvertiert->w;
+	small_text_rect.h = small_text_konvertiert->h;
+
+
 	SDL_BlitSurface(text_konvertiert, NULL, surface, &text_rect);
-	SDL_UpdateWindowSurface(window);
+	SDL_BlitSurface(small_text_konvertiert, NULL, surface, &small_text_rect);
 	SDL_UpdateWindowSurface(window);
 
 	SDL_Event event;
@@ -117,6 +133,7 @@ int SDL_covers(int posit_in_game) {
 	}
 	SDL_FreeSurface(text_konvertiert);
     TTF_CloseFont(font);
+	TTF_CloseFont(small_font);
     TTF_Quit();
 	return decision;
 }
