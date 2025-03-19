35d5cf5e898d92be30be37d75ac6a5813b1cd80f
Vitalii Heneha <genlosha@gmail.com>
2025-03-19 12:11:54 +0100

    removed old play.c, changed order of SDL render

--- SDL_render.c
+++ SDL_render.c
@@ -8,11 +8,15 @@ int SDL_render (piece_t board [8][8]) {
 
    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 0, 0));
 
-    if (SDL_init_board() != 0 || SDL_init_text() != 0) {
+    if (SDL_init_board() != 0) {
         return 1;
     }
 
     SDL_pieces(board);
+
+    if (SDL_init_text() != 0) {
+        return 1;
+    }
 
     SDL_UpdateWindowSurface(window);
 
